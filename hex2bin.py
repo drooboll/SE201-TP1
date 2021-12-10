64e3964555c1d2873528d85329034fb422638fbc
Valery <drobotun.mail@gmail.com>
2021-12-10 18:40:33 +0700

    Binary instructions

---
+++ hex2bin.py
@@ -0,0 +1,18 @@
+input = open('instructions.txt', 'r')
+output = open ('bin-instr.txt', 'w')
+
+lines = input.readlines()
+input.close()
+
+for line in lines:
+    splitted = line.split("\t")
+    intval = int(splitted[1], 16)
+    funct7 = (intval & (0b1111111 << 25)) >> 25
+    rs2 = (intval & (0b1111 << 20)) >> 20
+    rs1 = (intval & (0b1111 << 15)) >> 15
+    funct3 = (intval & (0b111 << 12)) >> 12
+    rd = (intval & (0b1111 << 7)) >> 7
+    opcode = intval & (0b111111)
+    output.write("{}\t{:07b} {:04b} {:04b} {:03b} {:04b} {:07b}\n".format(splitted[0], funct7, rs2, rs1, funct3, rd, opcode))
+
+output.close()
